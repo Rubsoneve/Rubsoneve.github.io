64a7da43da023b68ced52f28c0ca232fce8bef70
Rubsoneve <131001055+Rubsoneve@users.noreply.github.com>
2024-05-05 14:36:12 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,3 @@
-console.log = () => {}
-const { expect } = require('chai');
-const rewire = require('rewire');
-const Vue = require('vue');
-
-describe('', function () {
-    it('', function () {
-        let appModule = rewire("../js/app.js");
-        let vueApp = appModule.__get__("app");
-        expect(vueApp).to.exist();
-    });
-});
-       
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+}
